c5b625becfd159194b94a7227a392f98dabaeaca
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-11-27 13:33:25 +0530

    First Commit

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css',
+})
+export class TodoListComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+  removeTask(task: string) {
+    this.tasks = this.tasks.filter((t) => t !== task);
+  }
+}
